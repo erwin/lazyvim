e390b8b4b35b12cb0140eb560ad31a9e61032159
erwin <erwin@fastmail.org>
2026-05-18 11:10:36 +0700

    Make colorscheme light/dark detection more robust

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,48 @@
+-- 1. Helper function to query the system's current color scheme
+local function get_system_theme()
+  local handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme")
+  if not handle then
+    return "dark"
+  end
+
+  local result = handle:read("*a")
+  handle:close()
+
+  result = result:gsub("['%\n]", "")
+
+  if result == "prefer-light" or result == "default" then
+    return "light"
+  else
+    return "dark"
+  end
+end
+
+-- 2. Determine the theme mode on startup
+local system_mode = get_system_theme()
+
 return {
-  { "miikanissi/modus-themes.nvim" },
-  -- Configure LazyVim to load modus
+  -- Configure the modus-themes plugin options
+  {
+    "miikanissi/modus-themes.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("modus-themes").setup({
+        -- The modern configuration uses a 'variants' table to specify
+        -- preferences for individual themes
+        variants = {
+          modus_operandi = "default", -- options: default, tinted, deutan, tritan
+          modus_vivendi = "default",
+        },
+      })
+    end,
+  },
+
+  -- Configure LazyVim to load the correct variant
   {
     "LazyVim/LazyVim",
     opts = {
-        colorscheme = "modus",
+      colorscheme = system_mode == "light" and "modus_operandi" or "modus_vivendi",
     },
-  }
+  },
 }
