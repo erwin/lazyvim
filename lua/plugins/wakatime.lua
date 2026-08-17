c63a8bbd46f79983569ac862a878d0f60ac1ecf7
erwin <erwin@fastmail.org>
2026-08-17 12:46:27 +0700

    Skip wakatime on servers

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -1,4 +1,9 @@
 return {
   "wakatime/vim-wakatime",
   lazy = false,
+  -- Servers / install_gui=false do not export WAKATIME_API_KEY.
+  cond = function()
+    local key = vim.env.WAKATIME_API_KEY
+    return type(key) == "string" and key ~= ""
+  end,
 }
